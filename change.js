2ff9812905ae7faec4e1dddeb23cbbf7af5348a4
Varun Harsha <varunharsha10@gmail.com>
2024-09-14 19:41:45 +0530

    Version 1 restored

--- change.js
+++ change.js
@@ -1,6 +1,1 @@
 console.log('change')
-
-//omg
-
-
-//this is the only chnage in version 3
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
 //hey this is a boring index.js file
-
-console.log('index.js')
-console.log("version 2")
-
-//hahahahahah
